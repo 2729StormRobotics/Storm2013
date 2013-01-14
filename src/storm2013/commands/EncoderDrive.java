cd3cdbc44fffbc99fe2062ec285b812dab0d472e
evan1026 <kkrockme4@gmail.com>
2013-01-14 17:49:25 -0500

    Started making javadocs

--- src/storm2013/commands/EncoderDrive.java
+++ src/storm2013/commands/EncoderDrive.java
@@ -9,13 +9,16 @@ import storm2013.Robot;
 import storm2013.subsystems.DriveTrain;
 
 /**
- *
+ *Drives a certain number of wheel rotations based on an encoder. The encoder values
+ * are supplied by the {@link IDriveTrainEncoder}, which is implemented by some other class
+ * to supply values in rotations. It then drives at the specified drive speed until
+ * until it reaches the goal.
+ * 
  * @author evan1026
  */
 
 public class EncoderDrive extends Command {
 
-    
     private double _goal;
     private double _dist;
     private double _driveSpeed;
@@ -24,6 +27,17 @@ public class EncoderDrive extends Command {
     private IDriveTrainEncoder _encoder;
     private DriveTrain _driveTrain = Robot.driveTrain;
     
+    /**
+     * Main constructor for the class. More could be created if it proves to be worthwhile,
+     * but for now, use this one.
+     * 
+     * @param goal            How far you want to go (use rotations for units, just to make a standard).
+     * @param driveSpeed      The speed at which you want to travel (also in rotations).
+     * @param encoder         The class that implements {@link IDriveTrainEncoder}.
+     * @param decelThreshold  The threshold to determine when to start to decelerate.
+     * @param stopThreshold   The threshold to determine when to stop the command.
+     * @see   IDriveTrainEncoder
+     */
     public EncoderDrive(double goal, double driveSpeed, IDriveTrainEncoder encoder,
             double decelThreshold, double stopThreshold){ 
         _goal = goal;
@@ -34,9 +48,19 @@ public class EncoderDrive extends Command {
         requires(Robot.driveTrain);
     }
     
+    /**
+     * Implemented from {@link Command}, but it doesn't actually do anything at the moment.
+     */
      protected void initialize() {
     }
     
+    /**
+     * Implemented from {@link Command}. It is run over and over and such, and it calculates
+     * what the drive speed the robot needs to accelerate toward by setting values in 
+     * the {@link DriveTrain}.
+     * 
+     * @see DriveTrain
+     */
     public void execute() {
         _dist = _encoder.getDistance();
         
@@ -52,15 +76,103 @@ public class EncoderDrive extends Command {
             _driveTrain.tankDrive(0, 0);
         }  
     }
+    
+    /**
+     * Implemented from {@link Command}. When it returns true, it runs {@link #end()},
+     * and then stops running the command.
+     * 
+     * @return true if it is within the stopping threshold ({@link #_stopThreshold}) and false otherwise.
+     */
     public boolean isFinished() {
         return (Math.abs(_goal) - Math.abs(_dist) <= _stopThreshold);
     }
+    /**
+     * Implemented from {@link Command}. This is the last thing run before the command
+     * stops running. At the moment, it doesn't actually do anything.
+     */
     public void end() {
     }
     
+    /**
+     * Implemented from {@link Command}. When {@link Command#cancel()} is run, 
+     * this is first called, then {@link #end()}.
+     */
     public void interrupted() {
     }
 
+    /**
+     * Gets the current goal.
+     * @return The current goal, in rotations.
+     */
+    public double getGoal() {
+        return _goal;
+    }
+    
+    /**
+     * Sets a new goal.
+     * @param goal The new goal, in rotations.
+     */
+    public void setGoal(double goal) {
+        _goal = goal;
+    }
+    
+    /**
+     * Gets the total distance traveled.
+     * @return The distance traveled, in revolutions.
+     */
+    public double getDist() {
+        return _dist;
+    }
    
+    //No set distance. You should implement that in the encoder.
+   
+    /**
+     * Returns the speed at which the robot it set to drive.
+     * @return The drive speed, in RPM.
+     */
+    public double getDriveSpeed() {
+        return _driveSpeed;
+    }
+    
+    /**
+     * Sets the drive speed.
+     * @param driveSpeed The drive speed, in RPM.
+     */
+    public void setDriveSpeed(double driveSpeed) {
+        _driveSpeed = driveSpeed;
+    }
+    
+    /**
+     * Gets the threshold at which the robot will start to decelerate.
+     * @return The threshold, a distance, in revolutions.
+     */
+    public double getDecelThreshold() {
+        return _decelThreshold;
+    }
+    
+    /**
+     * Sets a new threshold to decide when the robot will decelerate.
+     * @param decelThreshold The new deceleration threshold, in revolutions.
+     */
+    public void setDecelThreshold(double decelThreshold) {
+        _decelThreshold = decelThreshold;
+    }
+    
+    /**
+     * Gets the threshold at which the robot will stop executing the command.
+     * @return The threshold, in revolutions.
+     */
+    public double getStopThreshold() {
+        return _stopThreshold;
+    }
+    
+    /**
+     * Sets a new threshold at which the robot will stop.
+     * @param stopThreshold The new threshold, in revolutions
+     */
+    public void setStopThreshold(double stopThreshold) {
+        _stopThreshold = stopThreshold;
+    }
+    
     
 }
--- src/storm2013/commands/IDriveTrainEncoder.java
+++ src/storm2013/commands/IDriveTrainEncoder.java
@@ -5,9 +5,16 @@
 package storm2013.commands;
 
 /**
- *
+ * An interface that needs to be implemented in order for {@link EncoderDrive} to work.
+ * It simply supplies the distance traveled based on encoder values.
+ * 
  * @author evan1026
  */
 public interface IDriveTrainEncoder {
+   
+    /**
+     * Will calculate the total distance traveled based on encoder data
+     * @return The total distance traveled, in revolutions.
+     */
     public double getDistance();
 }

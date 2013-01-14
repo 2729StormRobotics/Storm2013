be5a61c1f2154cfcbd4cddafb6d2fd001d79d9dd
evan1026 <kkrockme4@gmail.com>
2013-01-14 16:14:18 -0500

    EncoderDrive now finally has something working nicely. Also changed DriveTrain so that it uses the AccelerationLimiter in TankDrive, which makes things nicer for us, since we don't have to deal with it

--- src/storm2013/commands/EncoderDrive.java
+++ src/storm2013/commands/EncoderDrive.java
@@ -4,8 +4,6 @@
  */
 package storm2013.commands;
 
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Command;
 import storm2013.Robot;
 import storm2013.subsystems.DriveTrain;
@@ -21,42 +19,41 @@ public class EncoderDrive extends Command {
     private double _goal;
     private double _dist;
     private double _driveSpeed;
+    private double _decelThreshold;
+    private double _stopThreshold;
     private IDriveTrainEncoder _encoder;
     private DriveTrain _driveTrain = Robot.driveTrain;
     
-    public EncoderDrive(double goal, double driveSpeed,IDriveTrainEncoder encoder){ 
+    public EncoderDrive(double goal, double driveSpeed, IDriveTrainEncoder encoder,
+            double decelThreshold, double stopThreshold){ 
         _goal = goal;
         _driveSpeed = driveSpeed;
         _encoder = encoder;
+        _decelThreshold = decelThreshold;
+        _stopThreshold = stopThreshold;
         requires(Robot.driveTrain);
     }
     
+     protected void initialize() {
+    }
     
     public void execute() {
         _dist = _encoder.getDistance();
         
-        if (Math.abs(_dist) < Math.abs(_goal)){
-            if (_goal > 0){
+        if(Math.abs(_goal) - Math.abs(_dist) > _decelThreshold){
+            if (_dist < _goal){
                 _driveTrain.tankDrive(_driveSpeed, _driveSpeed);
             }
-            else{
+            else if (_dist > _goal){
                 _driveTrain.tankDrive(-_driveSpeed, -_driveSpeed);
-            }
-        }
-        else if (Math.abs(_dist) > Math.abs(_goal)){
-            if (_goal > 0){
-                _driveTrain.tankDrive(-_driveSpeed, -_driveSpeed);
-            }
-            else{
-                _driveTrain.tankDrive(_driveSpeed, _driveSpeed);
             }
         }
         else{
             _driveTrain.tankDrive(0, 0);
-        }
+        }  
     }
     public boolean isFinished() {
-        return (_dist == _goal);
+        return (Math.abs(_goal) - Math.abs(_dist) <= _stopThreshold);
     }
     public void end() {
     }
@@ -64,8 +61,6 @@ public class EncoderDrive extends Command {
     public void interrupted() {
     }
 
-    protected void initialize() {
-        
-    }
+   
     
 }
--- src/storm2013/subsystems/DriveTrain.java
+++ src/storm2013/subsystems/DriveTrain.java
@@ -9,14 +9,22 @@ import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import storm2013.commands.ArcadeDrive;
+import storm2013.utilities.AccelerationLimiter;
 
 /**
  *
  * @author Joe
  */
 public class DriveTrain extends Subsystem {
+    
+    private double ACCEL_RATE = 1;
+    private double DECEL_RATE = 1;
+    
     Victor _left,_right;
-    RobotDrive _drive = new RobotDrive(_left,_right);
+    AccelerationLimiter _leftControl = new AccelerationLimiter(_left, ACCEL_RATE, DECEL_RATE);
+    AccelerationLimiter _rightControl = new AccelerationLimiter(_right, ACCEL_RATE, DECEL_RATE);
+    RobotDrive _drive = new RobotDrive(_leftControl,_rightControl);
+    
 
     public DriveTrain() {
         LiveWindow.addActuator("Drive Train", "Left Motor", _left);

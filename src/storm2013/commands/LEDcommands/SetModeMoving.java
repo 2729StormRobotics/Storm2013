7f0f2f37a554f89d552fb18d51df26222bc3bef6
Joe Doyle (Ginto8) <ginto8@gmail.com>
2013-04-11 09:40:53 -0400

    Prelim alliance-colored LED

--- src/storm2013/commands/LEDcommands/SetModeMoving.java
+++ src/storm2013/commands/LEDcommands/SetModeMoving.java
@@ -1,5 +1,7 @@
 package storm2013.commands.LEDcommands;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import storm2013.Robot;
@@ -19,6 +21,17 @@ public class SetModeMoving extends Command {
     }
     
     public void execute() {
+        Alliance alliance = DriverStation.getInstance().getAlliance();
+        double r = 0,g = 0,b = 0;
+        if(alliance.value == Alliance.kRed_val) {
+            r = 1;
+            g = b = 0;
+        } else if(alliance.value == Alliance.kBlue_val) {
+            b = 1;
+            r = g = 0;
+        } else {
+            r = g = b = 1;
+        }
         double rate = 2*Math.PI*(Math.abs(Robot.driveTrain.getLeftSpeed()) + Math.abs(Robot.driveTrain.getRightSpeed())) / 2;
         if(rate == 0) {
             _val = Math.PI/2;
@@ -33,8 +46,8 @@ public class SetModeMoving extends Command {
         } else {
             scalar = (scalar-0.25)/0.75;
         }
-        int color = (int)(scalar*255);
-        Robot.ledStrip.setColor(color,color,color);
+        double color = scalar*255;
+        Robot.ledStrip.setColor((int)(r*color),(int)(g*color),(int)(b*color));
     }
     protected void initialize() {
         _timer.reset();

533e314fb70560787bd4d6232e61e50ea6325a55
Joe Doyle (Ginto8) <ginto8@gmail.com>
2013-10-27 13:59:51 -0400

    Map things to one gamepad

--- src/storm2013/OI.java
+++ src/storm2013/OI.java
@@ -24,6 +24,7 @@ import storm2013.commands.SpinUp;
 import storm2013.commands.TargetPIDTurn;
 import storm2013.commands.TiltForDistance;
 import storm2013.commands.TiltToCurrentDistance;
+import storm2013.subsystems.Shooter;
 import storm2013.subsystems.Tilter;
 import storm2013.subsystems.Vision;
 import storm2013.utilities.Target;
@@ -31,35 +32,35 @@ import storm2013.utilities.Target;
 /** Connects the gamepads/joysticks to actual functionality on the robot. */
 public class OI {    
     private Joystick driveJoystick = new Joystick(RobotMap.PORT_JOYSTICK_DRIVE),
-                     shootJoystick = new Joystick(RobotMap.PORT_JOYSTICK_SHOOT),
+//                     shootJoystick = new Joystick(RobotMap.PORT_JOYSTICK_SHOOT),
                      debugJoystick = new Joystick(RobotMap.PORT_JOYSTICK_DEBUG);
     
-    private Button recordEncoderButton = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_PRINT_ENCODER),
+    private Button //recordEncoderButton = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_PRINT_ENCODER),
                    slowModeButton      = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_SLOW),
-                   rainbowButton       = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_RAINBOW),
+                   //rainbowButton       = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_RAINBOW),
                    spazButton          = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_SPAZ),
-                   target2ptTurnButton = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_TARGET_2PT),
-                   target3ptTurnButton = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_TARGET_3PT),
-                   shootButton         = new JoystickButton(shootJoystick, RobotMap.JOYSHOOT_BUTTON_SHOOT),
-                   shootFullButton     = new JoystickButton(shootJoystick, RobotMap.JOYSHOOT_BUTTON_SHOOT_FULLCOURT),
-                   spinDownButton      = new JoystickButton(shootJoystick, RobotMap.JOYSHOOT_BUTTON_SPIN_DOWN),
-                   spinDown2Button     = new JoystickButton(shootJoystick, RobotMap.JOYSHOOT_BUTTON_SPIN_DOWN2),
-                   target2ptTiltButton = new JoystickButton(shootJoystick, RobotMap.JOYSHOOT_BUTTON_TARGET_2PT),
-                   target3ptTiltButton = new JoystickButton(shootJoystick, RobotMap.JOYSHOOT_BUTTON_TARGET_3PT),
-                   tomahawkButton      = new JoystickButton(shootJoystick, RobotMap.JOYSHOOT_BUTTON_TOMAHAWK),
-                   tomahawkBackButton  = new JoystickButton(shootJoystick, RobotMap.JOYSHOOT_BUTTON_TOMAHAWK_BACK),
-                   nextDistanceButton  = new Button() {
-                       public boolean get() {
-                           return shootJoystick.getRawAxis(RobotMap.JOYSHOOT_AXIS_DISTANCE) > 0.1;
-                       }
-                   },
-                   prevDistanceButton  = new Button() {
-                       public boolean get() {
-                           return shootJoystick.getRawAxis(RobotMap.JOYSHOOT_AXIS_DISTANCE) < -0.1;
-                       }
-                   },
-                   tiltToDistanceButton  = new JoystickButton(shootJoystick,RobotMap.JOYSHOOT_BUTTON_TILTTODIST),
-                   logPotButton          = new JoystickButton(shootJoystick,RobotMap.JOYSHOOT_BUTTON_LOGPOT),
+                   //target2ptTurnButton = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_TARGET_2PT),
+                   //target3ptTurnButton = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_TARGET_3PT),
+                   shootButton         = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_SHOOT),
+                   //shootFullButton     = new JoystickButton(shootJoystick, RobotMap.JOYSHOOT_BUTTON_SHOOT_FULLCOURT),
+                   spinDownButton      = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_SPIN_DOWN),
+                   //spinDown2Button     = new JoystickButton(shootJoystick, RobotMap.JOYSHOOT_BUTTON_SPIN_DOWN2),
+//                   target2ptTiltButton = new JoystickButton(shootJoystick, RobotMap.JOYSHOOT_BUTTON_TARGET_2PT),
+//                   target3ptTiltButton = new JoystickButton(shootJoystick, RobotMap.JOYSHOOT_BUTTON_TARGET_3PT),
+                   tomahawkButton      = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_TOMAHAWK),
+                   tomahawkBackButton  = new JoystickButton(driveJoystick, RobotMap.JOYDRIVE_BUTTON_TOMAHAWK_BACK),
+//                   nextDistanceButton  = new Button() {
+//                       public boolean get() {
+//                           return shootJoystick.getRawAxis(RobotMap.JOYSHOOT_AXIS_DISTANCE) > 0.1;
+//                       }
+//                   },
+//                   prevDistanceButton  = new Button() {
+//                       public boolean get() {
+//                           return shootJoystick.getRawAxis(RobotMap.JOYSHOOT_AXIS_DISTANCE) < -0.1;
+//                       }
+//                   },
+//                   tiltToDistanceButton  = new JoystickButton(shootJoystick,RobotMap.JOYSHOOT_BUTTON_TILTTODIST),
+//                   logPotButton          = new JoystickButton(shootJoystick,RobotMap.JOYSHOOT_BUTTON_LOGPOT),
                    feederAwayButton   = new Button() {
                        public boolean get() {
                            return driveJoystick.getRawAxis(RobotMap.JOYDRIVE_AXIS_FEEDERTURN) < -0.1;
@@ -73,11 +74,11 @@ public class OI {
                    controlShootButton = new JoystickButton(debugJoystick,RobotMap.JOYDEBUG_BUTTON_CONTROLSHOOT);
     
     public OI() {
-        shootButton        .whenPressed(new Shoot());
-        shootFullButton    .whenPressed(new Shoot(SpinUp.SPEED_FULLCOURT));
+        shootButton        .whenPressed(new Shoot(SpinUp.SPEED_SLOW));
+//        shootFullButton    .whenPressed(new Shoot(SpinUp.SPEED_FULLCOURT));
         spinDownButton     .whenPressed(new SpinDown());
-        spinDown2Button    .whenPressed(new SpinDown());
-        recordEncoderButton.whenPressed(new PrintAutonomousMove(0.6, 0.5));
+//        spinDown2Button    .whenPressed(new SpinDown());
+//        recordEncoderButton.whenPressed(new PrintAutonomousMove(0.6, 0.5));
         tomahawkButton     .whenPressed(new SpinTomahawk(true));
         tomahawkBackButton .whenPressed(new SpinTomahawk(false));
         feederAwayButton   .whenPressed(new FeederTurn(false));
@@ -91,40 +92,40 @@ public class OI {
         SmartDashboard.putData("Turn 3pt PID",turn3ptAim.getPIDController());
         SmartDashboard.putData("Tilt 3pt PID",tilt3ptAim.getPIDController());
         
-        target2ptTurnButton.whileHeld(turn2ptAim);
-        target3ptTurnButton.whileHeld(turn3ptAim);
-        target2ptTiltButton.whileHeld(tilt2ptAim);
-        target3ptTiltButton.whenPressed(new TiltToCurrentDistance(Tilter.SPEED_DEFAULT));
+//        target2ptTurnButton.whileHeld(turn2ptAim);
+//        target3ptTurnButton.whileHeld(turn3ptAim);
+//        target2ptTiltButton.whileHeld(tilt2ptAim);
+//        target3ptTiltButton.whenPressed(new TiltToCurrentDistance(Tilter.SPEED_DEFAULT));
         
-        nextDistanceButton.whenPressed(new Command() {
-            {
-                requires(Robot.vision);
-            }
-            protected void initialize() {
-                Robot.vision.nextDistance();
-            }
-            protected void execute() {}
-            protected boolean isFinished() {
-                return true;
-            }
-            protected void end() {}
-            protected void interrupted() {}
-        });
-        prevDistanceButton.whenPressed(new Command() {
-            {
-                requires(Robot.vision);
-            }
-            protected void initialize() {
-                Robot.vision.prevDistance();
-            }
-            protected void execute() {}
-            protected boolean isFinished() {
-                return true;
-            }
-            protected void end() {}
-            protected void interrupted() {}
-        });
-        tiltToDistanceButton.whenPressed(new TiltForDistance(Tilter.SPEED_DEFAULT,Vision.Distance.NEAR));
+//        nextDistanceButton.whenPressed(new Command() {
+//            {
+//                requires(Robot.vision);
+//            }
+//            protected void initialize() {
+//                Robot.vision.nextDistance();
+//            }
+//            protected void execute() {}
+//            protected boolean isFinished() {
+//                return true;
+//            }
+//            protected void end() {}
+//            protected void interrupted() {}
+//        });
+//        prevDistanceButton.whenPressed(new Command() {
+//            {
+//                requires(Robot.vision);
+//            }
+//            protected void initialize() {
+//                Robot.vision.prevDistance();
+//            }
+//            protected void execute() {}
+//            protected boolean isFinished() {
+//                return true;
+//            }
+//            protected void end() {}
+//            protected void interrupted() {}
+//        });
+//        tiltToDistanceButton.whenPressed(new TiltForDistance(Tilter.SPEED_DEFAULT,Vision.Distance.NEAR));
         
         controlShootButton.whileHeld(new Command() {
             {
@@ -140,43 +141,43 @@ public class OI {
             protected void end() {}
             protected void interrupted() {}
         });
-        rainbowButton.whileHeld(new SetModeRainbowDanceParty());
+//        rainbowButton.whileHeld(new SetModeRainbowDanceParty());
         spazButton.whileHeld(new Spaz());
-        logPotButton.whenPressed(new Command() {
-            private FileConnection file = null;
-            BufferedWriter out;
-            {
-                try {
-                    file = (FileConnection) Connector.open("file:///Pot.log", Connector.READ_WRITE);
-
-                    if(!file.exists()) {
-                        file.create();
-                    }
-
-                    OutputStream output = file.openOutputStream(Long.MAX_VALUE);
-                    out = new BufferedWriter(new OutputStreamWriter(output));
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-
-            
-            protected void initialize() {
-                String text = Robot.vision.getCurrDistance().getValue() + ":" + Robot.tilter.readStringPot();
-                try {
-                    out.write(text+"\n");
-                    out.flush();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-            protected void execute() {}
-            protected boolean isFinished() {
-                return true;
-            }
-            protected void end() {}
-            protected void interrupted() {}
-        });
+//        logPotButton.whenPressed(new Command() {
+//            private FileConnection file = null;
+//            BufferedWriter out;
+//            {
+//                try {
+//                    file = (FileConnection) Connector.open("file:///Pot.log", Connector.READ_WRITE);
+//
+//                    if(!file.exists()) {
+//                        file.create();
+//                    }
+//
+//                    OutputStream output = file.openOutputStream(Long.MAX_VALUE);
+//                    out = new BufferedWriter(new OutputStreamWriter(output));
+//                } catch (Exception ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//
+//            
+//            protected void initialize() {
+//                String text = Robot.vision.getCurrDistance().getValue() + ":" + Robot.tilter.readStringPot();
+//                try {
+//                    out.write(text+"\n");
+//                    out.flush();
+//                } catch (Exception ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//            protected void execute() {}
+//            protected boolean isFinished() {
+//                return true;
+//            }
+//            protected void end() {}
+//            protected void interrupted() {}
+//        });
     }
     
     // When a joystick is in its zero position, it will not necessarily read
@@ -194,7 +195,7 @@ public class OI {
     }
     
     public double getTilterAxis() {
-        return _zeroDeadzone(-shootJoystick.getRawAxis(RobotMap.JOYSHOOT_AXIS_TILTER),0.15);
+        return _zeroDeadzone(-driveJoystick.getRawAxis(RobotMap.JOYDRIVE_AXIS_TILTER),0.15);
     }
 }
 
--- src/storm2013/RobotMap.java
+++ src/storm2013/RobotMap.java
@@ -9,32 +9,33 @@ public class RobotMap {
                             PORT_JOYSTICK_DEBUG = 3;
     
     public static final int JOYDRIVE_BUTTON_SLOW            = 5;
-    public static final int JOYDRIVE_BUTTON_RAINBOW         = 6;
-    public static final int JOYDRIVE_BUTTON_SPAZ            = 8;
-    public static final int JOYDRIVE_BUTTON_PRINT_ENCODER   = 9;
-    public static final int JOYDRIVE_BUTTON_TARGET_2PT      = 1;
-    public static final int JOYDRIVE_BUTTON_TARGET_3PT      = 2;
+//    public static final int JOYDRIVE_BUTTON_RAINBOW         = 6;
+    public static final int JOYDRIVE_BUTTON_SPAZ            = 7;
+//    public static final int JOYDRIVE_BUTTON_PRINT_ENCODER   = 9;
+//    public static final int JOYDRIVE_BUTTON_TARGET_2PT      = 1;
+//    public static final int JOYDRIVE_BUTTON_TARGET_3PT      = 2;
     
-    public static final int JOYSHOOT_BUTTON_SHOOT           = 6;
-    public static final int JOYSHOOT_BUTTON_SHOOT_FULLCOURT = 5;
-    public static final int JOYSHOOT_BUTTON_SPIN_DOWN       = 8;
-    public static final int JOYSHOOT_BUTTON_SPIN_DOWN2      = 7;
-    public static final int JOYSHOOT_BUTTON_TARGET_2PT      = 1;
-    public static final int JOYSHOOT_BUTTON_TARGET_3PT      = 2;
-    public static final int JOYSHOOT_BUTTON_TOMAHAWK_BACK   = 9;
-    public static final int JOYSHOOT_BUTTON_TOMAHAWK        = 10;
-    public static final int JOYSHOOT_BUTTON_TILTER_SAFETY_1 = 5;
-    public static final int JOYSHOOT_BUTTON_TILTER_SAFETY_2 = 7;
+    public static final int JOYDRIVE_BUTTON_SHOOT           = 6;
+//    public static final int JOYSHOOT_BUTTON_SHOOT_FULLCOURT = 5;
+    public static final int JOYDRIVE_BUTTON_SPIN_DOWN       = 8;
+//    public static final int JOYSHOOT_BUTTON_SPIN_DOWN2      = 7;
+//    public static final int JOYSHOOT_BUTTON_TARGET_2PT      = 1;
+//    public static final int JOYSHOOT_BUTTON_TARGET_3PT      = 2;
+    public static final int JOYDRIVE_BUTTON_TOMAHAWK_BACK   = 9;
+    public static final int JOYDRIVE_BUTTON_TOMAHAWK        = 10;
+//    public static final int JOYSHOOT_BUTTON_TILTER_SAFETY_1 = 5;
+//    public static final int JOYSHOOT_BUTTON_TILTER_SAFETY_2 = 7;
     public static final int JOYSHOOT_BUTTON_TILTTODIST      = 3;
-    public static final int JOYSHOOT_BUTTON_LOGPOT          = 4;
+//    public static final int JOYSHOOT_BUTTON_LOGPOT          = 4;
     
     public static final int JOYDEBUG_BUTTON_CONTROLSHOOT = 10;
     
     public static final int JOYDRIVE_AXIS_DRIVE_LEFT  = 2,
                             JOYDRIVE_AXIS_DRIVE_RIGHT = 4,
                             JOYDRIVE_AXIS_FEEDERTURN  = 5,
-                            JOYSHOOT_AXIS_TILTER      = 2,
-                            JOYSHOOT_AXIS_DISTANCE    = 6,
+                            JOYDRIVE_AXIS_TILTER      = 6,
+//                            JOYSHOOT_AXIS_TILTER      = 2,
+//                            JOYSHOOT_AXIS_DISTANCE    = 6,
                             JOYDEBUG_AXIS_SHOOTSPEED  = 4;
     
     public static final int PORT_MOTOR_DRIVE_LEFT     = 1;
